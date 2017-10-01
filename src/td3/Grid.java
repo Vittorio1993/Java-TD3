2abdc02d248af47ff8e28d856c76dfda9d5429bf
Huakai ZHANG <zhanghuakai1234@gmail.com>
2017-10-02 00:06:35 +0200

    01/10/2017

--- src/td3/Grid.java
+++ src/td3/Grid.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package td3;
+import java.util.ArrayList;
 
 /**
  *
  * @author zhanghuakai
  */
 public class Grid {
+    protected ArrayList<Integer> lstgrid;
+    protected Integer mise;
+    public Grid(Integer m){
+    mise=m;
+    lstgrid=new ArrayList<>();
+}
+    public void gridtolo(Integer a,Integer b,Integer c,Integer d){
+        lstgrid.add(a);
+        lstgrid.add(b);
+        lstgrid.add(c);
+        lstgrid.add(d);
+    }
     
 }
